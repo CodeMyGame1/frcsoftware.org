a48743c892c6c1610f96d258091b5a1a5ba01db0
Daniel Chen <108989218+Daniel1464@users.noreply.github.com>
2026-07-16 23:38:10 -0400

    Add preliminary articles for commands V3 (#21)
    Co-authored-by: Adriana <adrianammassie@gmail.com>
    Co-authored-by: Sam Freund <samf.236@proton.me>
    Co-authored-by: Adriana <16786568+Adrianamm@users.noreply.github.com>

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -124,48 +124,38 @@ export const sidebarSections: Record<string, SidebarSection[]> = {
                             label: 'Stage 1B: Commands',
                             collapsed: true,
                             items: [
-                                // {
-                                //     label: 'Stage 1B Introduction',
-                                //     slug: 'learning-course/stage1b/overview',
-                                // },
-                                // {
-                                //     label: '1B',
-                                //     collapsed: true,
-                                //     items: [
-                                //         {
-                                //             label: 'The Concepts',
-                                //             slug: 'stage-1b-commands/command-based-overview',
-                                //         },
-                                //         {
-                                //             label: 'The Body of a Command',
-                                //             slug: 'stage-1b-commands/the-command-body',
-                                //         },
-                                //         {
-                                //             label: 'Commands & Mechanisms, Pt. 1',
-                                //             slug: 'stage-1b-commands/commands-and-mechanisms',
-                                //         },
-                                //         {
-                                //             label: 'Triggers and Scheduling',
-                                //             slug: 'stage-1b-commands/triggers',
-                                //         },
-                                //         {
-                                //             label: 'Commands & Mechanisms, Pt. 2',
-                                //             slug: 'stage-1b-commands/commands-and-mechanisms-pt2',
-                                //         },
-                                //         {
-                                //             label: 'Exercise - Kitbot Rewrite, Pt. 1',
-                                //             slug: 'stage-1b-commands/command-based-kitbot',
-                                //         },
-                                //         {
-                                //             label: 'Suppliers in Command-Based',
-                                //             slug: 'stage-1b-commands/suppliers-in-command-based',
-                                //         },
-                                //         {
-                                //             label: 'Exercise - Kitbot Rewrite, Pt. 2',
-                                //             slug: 'stage-1b-commands/command-based-kitbot-pt2',
-                                //         },
-                                //     ],
-                                // },
+                                {
+                                    label: 'The Concepts',
+                                    slug: 'learning-course/stage-1b-commands/command-based-overview',
+                                },
+                                {
+                                    label: 'The Body of a Command',
+                                    slug: 'learning-course/stage-1b-commands/the-command-body',
+                                },
+                                {
+                                    label: 'Commands & Mechanisms, Pt. 1',
+                                    slug: 'learning-course/stage-1b-commands/commands-and-mechanisms',
+                                },
+                                {
+                                    label: 'Triggers and Scheduling',
+                                    slug: 'learning-course/stage-1b-commands/triggers',
+                                },
+                                {
+                                    label: 'Commands & Mechanisms, Pt. 2',
+                                    slug: 'learning-course/stage-1b-commands/commands-and-mechanisms-pt2',
+                                },
+                                {
+                                    label: 'Exercise - Kitbot Rewrite, Pt. 1',
+                                    slug: 'learning-course/stage-1b-commands/command-based-kitbot',
+                                },
+                                {
+                                    label: 'Suppliers in Command-Based',
+                                    slug: 'learning-course/stage-1b-commands/suppliers-in-command-based',
+                                },
+                                {
+                                    label: 'Exercise - Kitbot Rewrite, Pt. 2',
+                                    slug: 'learning-course/stage-1b-commands/command-based-kitbot-pt2',
+                                },
                             ],
                         },
                     ],
